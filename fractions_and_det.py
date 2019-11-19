d6047611068f7cee9cd8948c2631552488b86d9c
piotr-kalemba <piotr-kalemba@wp.pl>
2019-11-19 14:00:30 +0100

    function for verifying solusions added

--- fractions_and_det.py
+++ fractions_and_det.py
@@ -26,6 +26,9 @@ class Fraction:
         numerator = self.numerator * other.denominator + other.numerator * self.denominator
         return Fraction(numerator, denominator)
 
+    def __iadd__(self, other):
+        return other.__add__(self)
+
     def __mul__(self, other):
         if type(other) == int:
             other = Fraction(other, 1)
@@ -33,13 +36,27 @@ class Fraction:
         numerator = self.numerator * other.numerator
         return Fraction(numerator, denominator)
 
+    def __sub__(self, other):
+        return -other + self
+
+    def __truediv__(self, other):
+        if other.numerator == 0:
+            raise ZeroDivisionError
+        return self * other.inverse()
+
+    def __neg__(self):
+        return self * (-1)
+
     def inverse(self):
         if self.numerator == 0:
             raise ZeroDivisionError
         return Fraction(self.denominator, self.numerator)
 
-    def opp_inv(self):
-        return self.inverse() * (-1)
+    def __eq__(self, other):
+        return self.numerator * other.denominator == self.denominator * other.numerator
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
 
     def is_zero(self):
         return self.numerator == 0
@@ -77,7 +94,7 @@ def det(a):
             sign = -sign
         for k in range(i + 1, n):
             if not a[k, i].is_zero():
-                a[k, :] = a[k, :] + a[i, :] * (a[k, i] * a[i, i].opp_inv())
+                a[k, :] = a[k, :] + a[i, :] * (a[k, i] / -a[i, i])
     return reduce(lambda x, y: x * y, np.diag(a)) * sign
 
 
@@ -102,20 +119,38 @@ def find_solutions(equations):
         print('The system is either inconsistent or has no unique solution.')
         return None
     denominator = det(a).inverse()
-    return np.array([str(det(inserted_column(a, i, b)) * denominator) for i in range(n)])
+    return np.array([det(inserted_column(a, i, b)) * denominator for i in range(n)])
+
+
+def multiply(a, v):
+    return [reduce(lambda x, y: x + y, row * v) for row in a]
+
+
+def check_solutions(equations, solutions):
+    for index, value in enumerate(multiply(equations[:, :-1], solutions), 0):
+        if value != equations[index, -1]:
+            return False
+    return True
+
+equations = np.array(
+    [Fraction(0, 1), Fraction(1, 2), Fraction(2, 1), Fraction(0, 1), Fraction(1, 1),
+     Fraction(1, 1), Fraction(3, 2), Fraction(-1, 1), Fraction(1, 1), Fraction(2, 1),
+     Fraction(2, 1), Fraction(1, 1), Fraction(6, 1), Fraction(0, 1), Fraction(3, 1),
+     Fraction(0, 1), Fraction(1, 1), Fraction(-1, 1), Fraction(1, 3), Fraction(4, 1)])
+equations = equations.reshape((4,5))
+
 
 def solve_system(equations):
     solutions = find_solutions(equations)
     for i in range(equations.shape[0]):
         print(f'x[{i}] = {solutions[i]}')
 
-equations = np.array(
-    [Fraction(0, 1), Fraction(1, 2), Fraction(2, 1), Fraction(0, 1), Fraction(1, 1),
-     Fraction(1, 1), Fraction(3, 2), Fraction(-1, 1), Fraction(1, 1), Fraction(2, 1),
-     Fraction(2, 1), Fraction(1, 1), Fraction(6, 1), Fraction(0, 1), Fraction(3, 1),
-     Fraction(0, 1), Fraction(1, 1), Fraction(-1, 1), Fraction(1, 3), Fraction(4, 1) ])
-equations = equations.reshape((4,5))
 
+solutions = find_solutions(equations)
 solve_system(equations)
+answer = check_solutions(equations, solutions)
 
-
+if answer:
+    print("The solutions are valid!")
+else:
+    print("The solutions are invalid!")

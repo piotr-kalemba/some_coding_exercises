0d5dd8b32a57019601b940cdbac5a22fce276a08
piotr-kalemba <piotr-kalemba@wp.pl>
2019-12-12 23:04:35 +0100

    sorting algorithms added

--- sorting_algorithms.py
+++ sorting_algorithms.py
@@ -4,12 +4,13 @@ from random import randint
 def swap(a, i, j):
     a[i], a[j] = a[j], a[i]
 
-
+# selection_sort
 def selection_sort(a):
     for i in range(len(a) - 1):
         swap(a, i, i + a[i:].index(min(a[i:])))
 
 
+# insertion_sort
 def insertion_sort(a):
     for i in range(1, len(a)):
         key = a[i]
@@ -20,6 +21,7 @@ def insertion_sort(a):
         a[j+1] = key
 
 
+# quick_sort
 def partition(a, start, end):
     r = randint(start, end)
     swap(a, r, end)
@@ -44,6 +46,7 @@ def quick_sort(a):
     quick(a, 0, len(a) - 1)
 
 
+# merge_sort
 def merge(a, b, start, middle, stop):
     left = start
     right = middle
@@ -71,6 +74,7 @@ def merge_sort(a):
     merge_recursively(a, b, 0, len(a))
 
 
+# count_sort
 def create_equal(a, m):
     return [a.count(i) for i in range(m)]
 
@@ -89,3 +93,52 @@ def count_sort(a, m):
         result[index] = a[i]
         next[value] += 1
     return result
+
+
+# heap_sort
+def left_child(i):
+    return 2 * i + 1
+
+
+def right_child(i):
+    return 2 * i + 2
+
+
+def swap_downwards(a, i, stop):
+    to_swap = i
+    if left_child(i) < stop and a[i] < a[left_child(i)]:
+        to_swap = left_child(i)
+    if right_child(i) < stop and a[to_swap] < a[right_child(i)]:
+        to_swap = right_child(i)
+    if to_swap != i:
+        swap(a, i, to_swap)
+        return to_swap
+    return -1
+
+
+def bubble_down(a, start, stop):
+    to_swap = start
+    while to_swap != -1:
+        to_swap = swap_downwards(a, to_swap, stop)
+
+
+def take_apart_heap(heap, stop):
+    while stop > 0:
+        swap(heap, 0, stop)
+        bubble_down(heap, 0, stop)
+        stop -= 1
+
+
+def make_heap(a):
+    start = len(a) - 1
+    while start >= 0:
+        bubble_down(a, start, len(a))
+        start -= 1
+    return a
+
+
+def heap_sort(a):
+    heap = make_heap(a)
+    take_apart_heap(heap, len(heap)-1)
+
+
